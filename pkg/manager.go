4ffcdb4b819d466f820ab683dfc087830b0937b8
lucasres <lucasresone@gmail.com>
2021-11-20 12:20:40 -0300

    add manager struct resolve #2

---
+++ pkg/manager.go
@@ -0,0 +1,54 @@
+package pkg
+
+import (
+	"errors"
+
+	"github.com/lucasres/db-stack/internal"
+)
+
+type Manager struct {
+	keyValues map[string]*internal.KeyValue
+}
+
+func NewManager() *Manager {
+	return &Manager{
+		keyValues: make(map[string]*internal.KeyValue, 0),
+	}
+}
+
+func (m *Manager) Set(k, v string) {
+	keyValue := internal.NewKeyValue(k)
+
+	keyValue.Push(v)
+
+	m.keyValues[k] = keyValue
+}
+
+func (m *Manager) Push(k, v string) error {
+	if kv, ok := m.keyValues[k]; ok {
+		kv.Push(v)
+	}
+
+	return errors.New("try push in undefined key")
+}
+
+func (m *Manager) Get(k string) (string, error) {
+	if kv, ok := m.keyValues[k]; ok {
+		return kv.Get(), nil
+	}
+
+	return "", errors.New("try get in undefined key")
+}
+
+func (m *Manager) Pop(k string) error {
+	if kv, ok := m.keyValues[k]; ok {
+		kv.Pop()
+		return nil
+	}
+
+	return errors.New("try pop in undefined key")
+}
+
+func (m *Manager) Flush() {
+	m.keyValues = make(map[string]*internal.KeyValue, 0)
+}
